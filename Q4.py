7803297a1c4c607fcc5486639e337f62c7455be3
malekan <immalekan@gmail.com>
2017-05-29 22:50:42 +0430

    find all urls, second commit

--- Q4.py
+++ Q4.py
@@ -1,9 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
+mainUrl = "http://meghdadit.com/"
+urls_to_scrape = []
 
+r = requests.get(mainUrl)
 
+soup = BeautifulSoup(r.text, "html.parser")
 
+for i in soup.select(".w25p"):
+    if i.find('a',href=True):
+        urls_to_scrape.append(i.find('a',href=True)['href'])
+
+for i in urls_to_scrape:
+    print(i)
 
 
 url_to_scrape = 'http://meghdadit.com/productlist/20/b.19/ipp.40/'
